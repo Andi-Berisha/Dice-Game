2cb3ebdc740e7ab0643b07c1694e2162f5dd651f
Andi Berisha <aberisha@qa.com>
2021-07-04 22:20:07 +0100

    roll function fixed

--- app.js
+++ app.js
@@ -1,68 +1,26 @@
-// ----- Variables ---
-
-const randomNumber1 = Math.floor(Math.random() * 6 + 1);
-
-const randomNumber2 = Math.floor(Math.random() * 6 + 1);
-
-console.log(randomNumber1);
-
-console.log(randomNumber2);
-
 // ----- App Functions ---
 
-function diceRoll1() {
-  if (randomNumber1 === 1) {
-    document.querySelector('.dice1').setAttribute('src', '/images/dice1.png');
-  }
-  if (randomNumber1 === 2) {
-    document.querySelector('.dice1').setAttribute('src', '/images/dice2.png');
-  }
-  if (randomNumber1 === 3) {
-    document.querySelector('.dice1').setAttribute('src', '/images/dice3.png');
-  }
-  if (randomNumber1 === 4) {
-    document.querySelector('.dice1').setAttribute('src', '/images/dice4.png');
-  }
-  if (randomNumber1 === 5) {
-    document.querySelector('.dice1').setAttribute('src', '/images/dice5.png');
-  }
-  if (randomNumber1 === 6) {
-    document.querySelector('.dice1').setAttribute('src', '/images/dice6.png');
-  }
+function rollDice() {
+  const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document
+    .querySelector('.dice1')
+    .setAttribute('src', `images/dice${randomNumber1}.png`);
+  document
+    .querySelector('.dice2')
+    .setAttribute('src', `images/dice${randomNumber2}.png`);
+
+  changeTitle(randomNumber1, randomNumber2);
 }
 
-function diceRoll2() {
-  if (randomNumber2 === 1) {
-    document.querySelector('.dice2').setAttribute('src', '/images/dice1.png');
-  }
-  if (randomNumber2 === 2) {
-    document.querySelector('.dice2').setAttribute('src', '/images/dice2.png');
-  }
-  if (randomNumber2 === 3) {
-    document.querySelector('.dice2').setAttribute('src', '/images/dice3.png');
-  }
-  if (randomNumber2 === 4) {
-    document.querySelector('.dice2').setAttribute('src', '/images/dice4.png');
-  }
-  if (randomNumber2 === 5) {
-    document.querySelector('.dice2').setAttribute('src', '/images/dice5.png');
-  }
-  if (randomNumber2 === 6) {
-    document.querySelector('.dice2').setAttribute('src', '/images/dice6.png');
-  }
-}
-
-function winner() {
-  diceRoll1();
-  diceRoll2();
-  if (randomNumber1 === randomNumber2) {
-    document.querySelector('#winner').textContent = `It's a Draw!`;
-  }
-  if (randomNumber1 > randomNumber2) {
-    document.querySelector('#winner').textContent = 'Player 1 wins';
-  }
-  if (randomNumber2 > randomNumber1) {
-    document.querySelector('#winner').textContent = 'Player 2 wins';
+function changeTitle(num1, num2) {
+  if (num1 === num2) {
+    document.querySelector('h1').innerHTML = 'Draw!';
+  } else if (num1 > num2) {
+    document.querySelector('h1').innerHTML = 'Player 1 Wins! 🚩';
+  } else {
+    document.querySelector('h1').innerHTML = 'Player 2 Wins! 🚩';
   }
 }
 
@@ -73,15 +31,12 @@ reset = document.querySelector('#reset');
 
 roll.addEventListener('click', (e) => {
   e.preventDefault();
-  winner();
+  rollDice();
 });
 
 reset.addEventListener('click', (e) => {
   e.preventDefault();
-  document.querySelector('#winner').innerText = 'End of the Road...';
-  document.querySelectorAll('.dice')[0].innerHTML =
-    '<p>Hit the Roll Again Button</p>';
-  document.querySelectorAll('.dice')[1].innerHTML = '<p></p>';
+  document.querySelector('h1').innerText = 'Feeling Lucky Punk!';
+  document.querySelector('.dice1').setAttribute('src', '/images/dice6.png');
+  document.querySelector('.dice2').setAttribute('src', '/images/dice6.png');
 });
-
-winner();
