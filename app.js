3b58a1567057da8107e93363fb9b491a2d0eaa09
Andi Berisha <aberisha@qa.com>
2021-07-04 20:43:44 +0100

    button functionality updated

--- app.js
+++ app.js
@@ -1,52 +1,68 @@
+// ----- Variables ---
+
+const randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+const randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+console.log(randomNumber1);
+
+console.log(randomNumber2);
+
 // ----- App Functions ---
 
 function diceRoll1() {
-  let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-
   if (randomNumber1 === 1) {
     document.querySelector('.dice1').setAttribute('src', '/images/dice1.png');
-  } else if (randomNumber1 === 2) {
+  }
+  if (randomNumber1 === 2) {
     document.querySelector('.dice1').setAttribute('src', '/images/dice2.png');
-  } else if (randomNumber1 === 3) {
+  }
+  if (randomNumber1 === 3) {
     document.querySelector('.dice1').setAttribute('src', '/images/dice3.png');
-  } else if (randomNumber1 === 4) {
+  }
+  if (randomNumber1 === 4) {
     document.querySelector('.dice1').setAttribute('src', '/images/dice4.png');
-  } else if (randomNumber1 === 5) {
+  }
+  if (randomNumber1 === 5) {
     document.querySelector('.dice1').setAttribute('src', '/images/dice5.png');
-  } else if (randomNumber1 === 6) {
+  }
+  if (randomNumber1 === 6) {
     document.querySelector('.dice1').setAttribute('src', '/images/dice6.png');
   }
-
-  return randomNumber1;
 }
 
 function diceRoll2() {
-  let randomNumber2 = Math.floor(Math.random() * 6 + 1);
   if (randomNumber2 === 1) {
     document.querySelector('.dice2').setAttribute('src', '/images/dice1.png');
-  } else if (randomNumber2 === 2) {
+  }
+  if (randomNumber2 === 2) {
     document.querySelector('.dice2').setAttribute('src', '/images/dice2.png');
-  } else if (randomNumber2 === 3) {
+  }
+  if (randomNumber2 === 3) {
     document.querySelector('.dice2').setAttribute('src', '/images/dice3.png');
-  } else if (randomNumber2 === 4) {
+  }
+  if (randomNumber2 === 4) {
     document.querySelector('.dice2').setAttribute('src', '/images/dice4.png');
-  } else if (randomNumber2 === 5) {
+  }
+  if (randomNumber2 === 5) {
     document.querySelector('.dice2').setAttribute('src', '/images/dice5.png');
-  } else if (randomNumber2 === 6) {
+  }
+  if (randomNumber2 === 6) {
     document.querySelector('.dice2').setAttribute('src', '/images/dice6.png');
   }
-  return randomNumber2;
 }
 
 function winner() {
-  if (diceRoll1() == diceRoll2()) {
+  diceRoll1();
+  diceRoll2();
+  if (randomNumber1 === randomNumber2) {
     document.querySelector('#winner').textContent = `It's a Draw!`;
   }
-  if (diceRoll1() > diceRoll2()) {
-    document.querySelector('#winner').textContent = 'Winner: Player 1';
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector('#winner').textContent = 'Player 1 wins';
   }
-  if (diceRoll1() < diceRoll2()) {
-    document.querySelector('#winner').textContent = 'Winner: Player 2';
+  if (randomNumber2 > randomNumber1) {
+    document.querySelector('#winner').textContent = 'Player 2 wins';
   }
 }
 
@@ -56,18 +72,16 @@ roll = document.querySelector('#roll');
 reset = document.querySelector('#reset');
 
 roll.addEventListener('click', (e) => {
-    e.preventDefault();
-    winner();
-  });
-
+  e.preventDefault();
+  winner();
+});
 
 reset.addEventListener('click', (e) => {
   e.preventDefault();
+  document.querySelector('#winner').innerText = 'End of the Road...';
   document.querySelectorAll('.dice')[0].innerHTML =
-    '<p>Hit the Roll Again button</p>';
-    document.querySelectorAll('.dice')[1].innerHTML =
-    '<p></p>';
+    '<p>Hit the Roll Again Button</p>';
+  document.querySelectorAll('.dice')[1].innerHTML = '<p></p>';
 });
 
-
-
+winner();
